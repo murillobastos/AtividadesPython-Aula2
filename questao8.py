6881f9c6ef4adba2af355367b22667976d57853c
Giovana Alves Magalhães <146298988+alvesmgiovana@users.noreply.github.com>
2023-10-28 11:43:55 -0300

    questão 8

---
+++ questao8.py
@@ -0,0 +1,11 @@
+num_inteiro1 = int (input ("Digite o primeiro número inteiro:"))
+num_inteiro2 = int (input ("Digite o segundo número inteiro:"))
+num_real = float (input("Digite um número real:"))
+
+resultado1 = (num_inteiro1 * 2) * (num_inteiro2 / 2)
+resultado2 = (num_inteiro1 * 3) + num_real
+resultado3 = (num_real ** 3)
+
+print("O produto do dobro do primeiro com metade do segundo é:",resultado1)
+print("A soma do triplo do primeiro com o terceiro é:",resultado2)
+print("O terceiro elevado ao cubo é:",resultado3)
