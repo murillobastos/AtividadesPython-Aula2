bbf08ee73492f81febc8406b01387af2047d5f8b
Giovana Alves Magalhães <146298988+alvesmgiovana@users.noreply.github.com>
2023-10-28 22:22:47 -0300

    questao4.py

---
+++ questao4.py
@@ -0,0 +1,5 @@
+lado = float(input("Digite o comprimento de um lado do quadrado: "))
+area = lado * lado
+dobro_da_area = 2 * area
+print("A área do quadrado é:", area)
+print("O dobro da área é:", dobro_da_area)
